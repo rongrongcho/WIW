0e09fcb8ad1dad10c9a76ee48ab17049085e3a39
Chorong Lee <mrarongsya@gmail.com>
2024-05-16 21:49:16 +0900

    Update setupProxy.js

--- wiw/setupProxy.js
+++ wiw/setupProxy.js
@@ -2,8 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
-      target: "http://localhost:8080/",
+    "/api", // 프록시할 요청 경로
+    createProxyMiddleware({
+      target: "http://localhost:8080", // Express 서버 주소
       changeOrigin: true,
     })
   );
